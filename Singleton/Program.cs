f36d6d757c23e89d4aafc745231599e9e9ac9efa
yogekgagdev <yogekgarg17@gmail.com>
2023-09-04 22:39:10 +0530

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿using SingletonPractise;
+
+Singleton s1 = new Singleton();
+s1.PrintMessage("This is first Message");
+
+Singleton s2 = new Singleton();
+s2.PrintMessage("This is second message");
+
+
+
+// See above we are creating 2 objects that's a very bad way of coding, lets say we were 
+// requring to use the objects in multiple classes, then againa and again memory creation is not
+// at all a good idea
+
+
+// SingletonImplementaion singletonImplementaion = new SingletonImplementaion(); 
+
+// due to private level access the above line 16 won't work
+
+SingletonImplementaion singletonImplementaion = SingletonImplementaion.GetInstance();
+singletonImplementaion.PrintMessge("SingleTonImplementaion Message1");
+
+SingletonImplementaion singletonImplementaion1 = SingletonImplementaion.GetInstance();
+singletonImplementaion.PrintMessge("SingleTonImplementaion Message2");
+
+Console.ReadLine();
---
+++ Singleton/SingletonImplementaion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPractise
+{
+    public sealed class SingletonImplementaion
+    {
+        private static SingletonImplementaion Instance = null;
+        private static int Counter =0;
+
+        public static SingletonImplementaion GetInstance() { 
+        
+            if (Instance == null)
+            {
+                Instance = new SingletonImplementaion();
+            }
+            return Instance;    
+        }
+
+        private SingletonImplementaion() {
+
+            Counter++;
+            Console.WriteLine("No of times SingletonImplementaion Class is initiated " + Counter);
+        }
+
+        public void PrintMessge(string message) { 
+        
+            Console.WriteLine(message); 
+        }
+    }
+}
